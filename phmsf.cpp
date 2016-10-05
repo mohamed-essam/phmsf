51c6712bada9f91b0c6d957e13b8f923cc1c994a
Mohamed Essam <mohamed.essam.arafa@gmail.com>
2016-10-05 19:31:49 +0200

    First functioning parallel version

--- phmsf.cpp
+++ phmsf.cpp
@@ -1,11 +1,12 @@
 // Copyright 2016 Mohamed Essam
+#include <omp.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include <algorithm>
 #include <utility>
 #include <vector>
-#include <map>
+#include <queue>
 
 #define PI 3.141569
 
@@ -13,12 +14,14 @@ struct Edge{
     unsigned int from, to, weight;
 };
 
-struct Graph{
-    Edge* edges;
-    int edgeCount;
+struct Tile{
+    std::vector<Edge> edges;
+    std::queue<Edge> tileQ;
+    unsigned int edgeCount;
+    int histogram[1800];
+    int start, end;
 };
 
-// Graph Creation
 int square(int a) {
     return a*a;
 }
@@ -28,71 +31,19 @@ int getEuclidianDistance(int r1, int g1, int b1, int r2, int g2, int b2) {
      square(g1 - g2) + square(b1 - b2)));
 }
 
-Graph* createGraph(unsigned char* rgb, unsigned int maxWeight,
-    unsigned int height, unsigned int width) {
-    unsigned int edgeCount = 2 * width * height - width - height;
-    Edge* graph = new Edge[edgeCount];
-    unsigned int nextEdge = 0;
-    unsigned int histogram[1800];
-    memset(histogram, 0, sizeof(histogram));
-    edgeCount = 0;
-    for (int j = 0; j < width; j++) {
-        for (int i = 0; i < height; i++) {
-            unsigned int idx1, idx2, idx3;
-            idx1 = (i*width + j) * 3;
-            idx3 = ((i + 1)*width + j) * 3;
-            if (i + 1 < height) {
-                unsigned int w = getEuclidianDistance(rgb[idx1], rgb[idx1 + 1],
-                    rgb[idx1 + 2], rgb[idx3], rgb[idx3 + 1], rgb[idx3 + 2]);
-                if (w <= maxWeight) {
-                    graph[nextEdge++] = { idx1 / 3, idx3 / 3, w };
-                    histogram[w]++;
-                    edgeCount++;
-                }
-            }
-            idx2 = (i*width + j + 1) * 3;
-            if (j + 1 < width) {
-                unsigned int w = getEuclidianDistance(rgb[idx1], rgb[idx1 + 1],
-                    rgb[idx1 + 2], rgb[idx2], rgb[idx2 + 1], rgb[idx2 + 2]);
-                if (w <= maxWeight) {
-                    graph[nextEdge++] = { idx1 / 3, idx2 / 3, w };
-                    histogram[w]++;
-                    edgeCount++;
-                }
-            }
-        }
-    }
-    for (int i = 1; i < 1800; i++) {
-        histogram[i] += histogram[i - 1];
-    }
-    Edge* sortedGraph = new Edge[edgeCount];
-    int st_pos = 0;
-    for (int i = 0; i < edgeCount; i++) {
-        int w = graph[i].weight;
-        if (w == 0) {
-            sortedGraph[st_pos++] = graph[i];
-        } else {
-            sortedGraph[histogram[w - 1]++] = graph[i];
-        }
-    }
-    delete[] graph;
-    Graph* ret = new Graph();
-    ret->edgeCount = edgeCount;
-    ret->edges = sortedGraph;
-    return ret;
-}
-
-// Initial Regions
 struct UnionFind{
     unsigned int* parent;
     unsigned int* rank;
+    bool* marked;
 
     explicit UnionFind(unsigned int size) {
         parent = new unsigned int[size];
         rank = new unsigned int[size];
+        marked = new bool[size];
         for (unsigned int i = 0; i < size; i++) {
             rank[i] = 1;
             parent[i] = i;
+            marked[i] = false;
         }
     }
 
@@ -123,93 +74,164 @@ struct UnionFind{
     }
 };
 
-struct Region{
-    unsigned int representative, size, credit;
-};
-
-struct RegionalData {
-    UnionFind* uf;
-    Region* regions;
-
-    RegionalData(UnionFind* _uf, Region* _regions) {
-        uf = _uf;
-        regions = _regions;
-    }
-};
-
-int computeCredit(int regionSize) {
-    return sqrt(4 * PI * regionSize);
+bool isInside(int a, int b, int c) {
+    return a >= b && a <= c;
 }
 
-RegionalData* getInitialRegions(Edge* graph, int minWeight, int minRegionSize,
-    int edgeCount, int width, int height) {
+double computeCredit(int size) {
+    return sqrt(4 * PI * size);
+}
+
+int* constructGraph(unsigned char* rgb, unsigned int height, unsigned int width,
+    unsigned int minWeight, unsigned int maxWeight, int& segmentCount) {
+    int threadCount = omp_get_max_threads();
+    Tile* tiles = new Tile[threadCount];
+    for (int i = 0; i < threadCount; i++) {
+        tiles[i].edgeCount = tiles[i].end = 0;
+        tiles[i].start = width*height;
+        memset(tiles[i].histogram, 0, sizeof(tiles[i].histogram));
+    }
+#pragma omp parallel for schedule(static, width*height / omp_get_max_threads())
+    for (int i = 0; i < width*height; i++) {
+        int th = omp_get_thread_num();
+        tiles[th].start = std::min(tiles[th].start, i);
+        tiles[th].end = std::max(tiles[th].end, i);
+        int j = i + 1;
+        unsigned int w1 = getEuclidianDistance(rgb[i * 3], rgb[i * 3 + 1],
+         rgb[i * 3 + 2], rgb[j * 3], rgb[j * 3 + 1], rgb[j * 3 + 2]);
+        if (w1 <= maxWeight && j % width > 0) {
+            tiles[th].edgeCount++;
+            tiles[th].edges.push_back({ i, j, w1 });
+            tiles[th].histogram[w1]++;
+        }
+        int k = i + width;
+        unsigned int w2 = getEuclidianDistance(rgb[i * 3], rgb[i * 3 + 1],
+         rgb[i * 3 + 2], rgb[k * 3], rgb[k * 3 + 1], rgb[k * 3 + 2]);
+        if (w2 <= maxWeight && k / width < height) {
+            tiles[th].edgeCount++;
+            tiles[th].edges.push_back({ i, k, w2 });
+            tiles[th].histogram[w2]++;
+        }
+    }
+#pragma omp parallel
+    {
+        int th = omp_get_thread_num();
+        for (int i = 1; i < 1800; i++) {
+            tiles[th].histogram[i] += tiles[th].histogram[i - 1];
+        }
+        int st_num = 0;
+        std::vector<Edge> temp(tiles[th].edgeCount);
+        for (int i = 0; i < tiles[th].edgeCount; i++) {
+            int w = tiles[th].edges[i].weight;
+            if (w == 0) {
+                temp[st_num++] = tiles[th].edges[i];
+            } else {
+                temp[tiles[th].histogram[w - 1]++] = tiles[th].edges[i];
+            }
+        }
+        tiles[th].edges.clear();
+        tiles[th].edges.shrink_to_fit();
+        tiles[th].edges = temp;
+    }
     UnionFind* uf = new UnionFind(width*height);
-    for (int i = 0; i < edgeCount && graph[i].weight <= minWeight; i++) {
-        if (graph[i].weight > minWeight)
-            continue;
-        uf->unite(graph[i].from, graph[i].to);
-    }
-    Region* regions = new Region[width*height];
-    for (int i = 0; i < width*height; i++) {
-        regions[i].size = regions[i].representative = regions[i].credit = 0;
-    }
-    for (int i = 0; i < width*height; i++) {
-        regions[uf->find(i)].size++;
-        regions[uf->find(i)].representative = uf->find(i);
-    }
-    for (int i = 0; i < width*height; i++) {
-        if (regions[i].size >= minRegionSize)
-            regions[i].credit = computeCredit(regions[i].size);
-        else
-            regions[i].credit = 1e9;
-    }
-    RegionalData* ret = new RegionalData(uf, regions);
-    return ret;
-}
-
-// Region Expansion
-
-struct SegmentationData{
-    int* segment;
-    int segmentCount;
-};
-
-SegmentationData expandRegions(RegionalData* rd, int minWeight,
-    int width, int height, Edge* graph, int edgeCount) {
-    for (int i = 0; i < edgeCount; i++) {
-        if (graph[i].weight <= minWeight) continue;
-        if (!rd->uf->isUnited(graph[i].from, graph[i].to)) {
-            int a = rd->uf->find(graph[i].from);
-            int b = rd->uf->find(graph[i].to);
-            int credit = std::min(rd->regions[a].credit, rd->regions[b].credit);
-            if (credit > graph[i].weight) {
-                int s = rd->uf->unite(a, b);
-                rd->regions[s].credit = credit - graph[i].weight;
+#pragma omp parallel
+    {
+        int th = omp_get_thread_num();
+        for (int i = 0; i < tiles[th].edgeCount &&
+         tiles[th].edges[i].weight <= minWeight; i++) {
+            if (isInside(tiles[th].edges[i].from,
+             tiles[th].start, tiles[th].end)
+                && isInside(tiles[th].edges[i].to,
+                 tiles[th].start, tiles[th].end)) {
+                uf->unite(tiles[th].edges[i].from, tiles[th].edges[i].to);
+            } else {
+                int survivor = uf->unite(tiles[th].edges[i].from,
+                 tiles[th].edges[i].to);
+                uf->marked[survivor] = true;
             }
         }
     }
-    int* repTable = new int[width*height];
-    int idx = 0;
-    for (int i = 0; i < width*height; i++) {
-        if (rd->uf->isRepresetative(i)) {
-            repTable[i] = idx++;
+    unsigned int* minEdgeWeight = new unsigned int[width*height];
+#pragma omp parallel
+    {
+        int th = omp_get_thread_num();
+        for (int i = 0; i < tiles[th].edgeCount; i++) {
+            if (!uf->isUnited(tiles[th].edges[i].from, tiles[th].edges[i].to)) {
+                minEdgeWeight[uf->find(tiles[th].edges[i].from)] =
+                std::min(minEdgeWeight[uf->find(tiles[th].edges[i].from)],
+                 tiles[th].edges[i].weight);
+                minEdgeWeight[uf->find(tiles[th].edges[i].to)] =
+                 std::min(minEdgeWeight[uf->find(tiles[th].edges[i].to)],
+                  tiles[th].edges[i].weight);
+            }
         }
     }
-    int* regionals = new int[width*height];
+    int* credit = new int[width*height];
+#pragma omp parallel for
     for (int i = 0; i < width*height; i++) {
-        regionals[i] = repTable[rd->uf->find(i)];
+        if (uf->isRepresetative(i)) {
+            credit[i] = computeCredit(uf->rank[i]) * minEdgeWeight[i];
+        }
     }
-    SegmentationData ret = { regionals, idx };
-    return ret;
-}
-
-// Main entry point
-
-SegmentationData segmentImage(unsigned char* rgb, int height,
-    int width, int minWeight, int maxWeight, int minRegionSize) {
-    Graph* graph = createGraph(rgb, maxWeight, height, width);
-    RegionalData* regional = getInitialRegions(graph->edges, minWeight,
-        minRegionSize, graph->edgeCount, width, height);
-    return expandRegions(regional, minWeight, width, height,
-        graph->edges, graph->edgeCount);
+#pragma omp parallel
+    {
+        int th = omp_get_thread_num();
+        for (int i = 0; i < tiles[th].edgeCount; i++) {
+            if (!(isInside(tiles[th].edges[i].from,
+             tiles[th].start, tiles[th].end) &&
+              isInside(tiles[th].edges[i].to,
+               tiles[th].start, tiles[th].end))) {
+                uf->marked[uf->find(tiles[th].edges[i].from)] =
+                uf->marked[uf->find(tiles[th].edges[i].from)] = true;
+            } else if (uf->marked[uf->find(tiles[th].edges[i].from)] &&
+             uf->marked[uf->find(tiles[th].edges[i].to)]) {
+                tiles[th].tileQ.push(tiles[th].edges[i]);
+            } else {
+                if (!uf->isUnited(tiles[th].edges[i].from,
+                 tiles[th].edges[i].to)) {
+                    int cr = std::min(credit[uf->find(tiles[th].edges[i].from)],
+                     credit[uf->find(tiles[th].edges[i].to)]);
+                    if (cr > tiles[th].edges[i].weight) {
+                        int sur = uf->unite(uf->find(tiles[th].edges[i].from),
+                         uf->find(tiles[th].edges[i].to));
+                        credit[sur] = cr - tiles[th].edges[i].weight;
+                        uf->marked[sur] =
+                        uf->marked[uf->find(tiles[th].edges[i].from)] |
+                         uf->marked[uf->find(tiles[th].edges[i].to)];
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i < threadCount; i++) {
+        while (tiles[i].tileQ.size()) {
+            if (!uf->isUnited(tiles[i].tileQ.front().from,
+             tiles[i].tileQ.front().to)) {
+                int cr = std::min(
+                credit[uf->find(tiles[i].tileQ.front().from)],
+                 credit[uf->find(tiles[i].tileQ.front().to)]);
+                if (cr > tiles[i].tileQ.front().weight) {
+                    int sur = uf->unite(
+                    uf->find(tiles[i].tileQ.front().from),
+                     uf->find(tiles[i].tileQ.front().to));
+                    credit[sur] = cr - tiles[i].tileQ.front().weight;
+                    uf->marked[sur] =
+                    uf->marked[uf->find(tiles[i].tileQ.front().from)] |
+                     uf->marked[uf->find(tiles[i].tileQ.front().to)];
+                }
+            }
+            tiles[i].tileQ.pop();
+        }
+    }
+    segmentCount = 0;
+    int* repTable = new int[width*height];
+    for (int i = 0; i < width*height; i++) {
+        if (uf->isRepresetative(i)) repTable[i] = segmentCount++;
+    }
+    int* segment = new int[width*height];
+#pragma omp parallel for
+    for (int i = 0; i < width*height; i++) {
+        segment[i] = repTable[uf->find(i)];
+    }
+    return segment;
 }
